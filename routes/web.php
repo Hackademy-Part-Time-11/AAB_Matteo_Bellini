6b060d0ecf3eda49e0b3dfdd7c4aba1812dc049a
bellinimatteo <bellinimatteo96@gmail.com>
2023-12-15 17:12:24 +0100

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AdminController;
+use App\Mail\RequestRoleMail;
 
 /*
 |--------------------------------------------------------------------------
